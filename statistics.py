1fb11e728c1020c4636ba22d0f42af7e5eb7d4b9
jari <jari.bjorne@utu.fi>
2017-01-10 22:22:36 +0200

    Working on visualizing the feature coverage.

--- statistics.py
+++ statistics.py
@@ -6,29 +6,49 @@ import matplotlib.pyplot as plt
 def getCounts(examples):
     lengths = [len(examples[x]) for x in ("sets", "ids")] + [examples[x].shape[0] for x in ("features", "labels")]
     assert len(set(lengths)) == 1, lengths
+    data = {}
+    print "Counting labels"
     counts = defaultdict(list)
     for sets, labels in zip(examples["sets"], examples["labels"]):
-        example = {}
         numLabels = numpy.count_nonzero(labels)
-        category = ",".join(sets)
-        counts[category].append(numLabels)
-    return counts
+        #category = ",".join(sets)
+        for category in sets:
+            counts[category].append(numLabels)
+    data["labels"] = counts
+    print "Counting features"
+    feature_names = examples["feature_names"]
+    for sets, features in zip(examples["sets"], examples["features"]):
+        feature_indices = numpy.nonzero(features)
+        for i in feature_indices:
+            name = feature_names[i]
+            if ":" in name:
+                tag = "coverage_" + name.split(":")[0]
+                if tag not in data:
+                    data[tag] = defaultdict(list)
+                counts = data[tag]
+                for category in sets:
+                    counts[category] += 1
+    return dict(data)
 
-def makeGraph(counts, outPath):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for key in sorted(counts.keys()):
-        values = sorted(counts[key])
-        total = float(len(values))
-        x = [x / total for x in reversed(range(len(values)))]
-        ax.plot(x, values)
-    plt.grid()
-    plt.legend()
-    plt.ylabel("labels")
-    plt.xlabel("proteins")
-    if outPath != None:
-        plt.savefig(outPath)
+def makeGraphs(data, outDir):
+    for key in sorted(data.keys()):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        counts = data[key]
+        for category in sorted(counts.keys()):
+            values = sorted(counts[category])
+            total = float(len(values))
+            x = [x / total for x in reversed(range(len(values)))]
+            ax.plot(x, values, label=key)
+        plt.grid()
+        plt.legend()
+        plt.ylabel("labels")
+        plt.xlabel("proteins")
+        if outDir != None:
+            figPath = os.path.join(outDir, key + ".pdf")
+            print "Saving figure to", figPath
+            plt.savefig(figPath)
     
 def makeStatistics(examples, outDir):
-    counts = getCounts(examples)
-    makeGraph(counts, os.path.join(outDir, "labels.pdf"))
+    data = getCounts(examples)
+    makeGraphs(data, outDir)

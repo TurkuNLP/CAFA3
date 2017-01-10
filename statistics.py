9bcadf2aa74dfed553fcd06060aa99382beece4c
jari <jari.bjorne@utu.fi>
2017-01-10 23:11:20 +0200

    Visualizing feature coverage seems to work.

--- statistics.py
+++ statistics.py
@@ -4,6 +4,12 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.dict_vectorizer import DictVectorizer
 
+def getCategories(sets):
+    categories = sets
+    if len(sets) == 1:
+        categories += [sets[0] + " only"]
+    return categories
+
 def getCounts(examples):
     lengths = [len(examples[x]) for x in ("sets", "ids")] + [examples[x].shape[0] for x in ("features", "labels")]
     assert len(set(lengths)) == 1, lengths
@@ -13,7 +19,7 @@ def getCounts(examples):
     for sets, labels in zip(examples["sets"], examples["labels"]):
         numLabels = numpy.count_nonzero(labels)
         #category = ",".join(sets)
-        categories = sets + ([",".join(sets)] if len(sets) > 1 else [])
+        categories = getCategories(sets)
         for category in categories:
             counts[category].append(numLabels)
     data["labels"] = counts
@@ -22,7 +28,7 @@ def getCounts(examples):
     dv.feature_names_ = examples["feature_names"]
     decoded = dv.inverse_transform(examples["features"])
     for sets, features in zip(examples["sets"], decoded):
-        categories = sets + ([",".join(sets)] if len(sets) > 1 else [])
+        categories = getCategories(sets)
         tags = [x.split(":")[0] for x in features.keys()]
         tagCounts = {x:tags.count(x) for x in set(tags)}
         for tag in tagCounts.keys():
@@ -55,7 +61,7 @@ def makeGraphs(data, outDir):
             x = [x / total for x in reversed(range(len(values)))]
             ax.plot(x, values, label=category + " n=" + str(len(values)))
         plt.grid()
-        plt.legend()
+        plt.legend(fontsize = 'x-small')
         plt.ylabel("labels")
         plt.xlabel("proteins")
         if outDir != None:

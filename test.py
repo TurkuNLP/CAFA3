16477e665216b1ed351ea5acdd0112d123550c73
jari <jari.bjorne@utu.fi>
2017-01-19 23:39:30 +0200

    Testing the warm start classifier parameter grid search.

--- test.py
+++ test.py
@@ -448,12 +448,16 @@ def evaluate(labels, predicted, label_names, label_size=None, terms=None):
 #     tp = labels.multiply(predicted)
 #     tp = labels.multiply(predicted)
 
-def learn(Cls, args, examples, trainSets, testSets, useMultiOutputClassifier, terms):
+def learn(Cls, args, examples, trainSets, testSets, useMultiOutputClassifier, terms, cls=None, verbose=True):
     print "Learning with args", args
-    cls = Cls(**args)
-    if useMultiOutputClassifier:
-        print "Using MultiOutputClassifier"
-        cls = MyMultiOutputClassifier(cls) # OneVsRestClassifier(cls)
+    if cls != None:
+        print "Initializing classifier", Cls.__name__, "with arguments", args
+        cls = Cls(**args)
+        if useMultiOutputClassifier:
+            print "Using MultiOutputClassifier"
+            cls = MyMultiOutputClassifier(cls) # OneVsRestClassifier(cls)
+    else:
+        print "Using existing classifier"
     print "Train / test = ", trainSets, "/", testSets
     sets = examples["sets"]
     trainIndices = [i for i in range(len(sets)) if any(x in trainSets for x in sets[i])]
@@ -474,78 +478,70 @@ def learn(Cls, args, examples, trainSets, testSets, useMultiOutputClassifier, te
         probabilities = cls.predict_proba(testFeatures)
     results = evaluate(testLabels, predicted, examples["label_names"], examples["label_size"], terms)
     print "Average:", metricsToString(results["average"])
-    print getResultsString(results, 20, ["average"])
+    if verbose:
+        print getResultsString(results, 20, ["average"])
     #if predictionsPath != None:
     #    predictionsPath(testIds, testLabels, predicted, examples["label_names"], predictionsPath)
     data = {"results":results, "args":args, "predicted":predicted, "gold":testLabels, "ids":testIds, "cafa_ids":testCafaIds, "probabilities":probabilities}
     if hasattr(cls, "feature_importances_"):
         data["feature_importances"] = cls.feature_importances_        
-    return data
+    return cls, data
+
+def warmStartGrid(Cls, classifierArgs, examples, terms):
+    for key in classifierArgs:
+        if key != "n_estimators" and len(args[key]) > 1:
+            raise Exception("Multiple classifier argument values defined for argument '" + str(key) + "'")
+    args = classifierArgs.copy()
+    numEstimatorList = sorted(args["n_estimators"])
+    args["n_estimators"] = [numEstimatorList[0]]
+    args = {x:args[x][0] for x in args.keys()}
+    cls = None
+    best = None
+    performances = []
+    for n in numEstimatorList:
+        if cls != None:
+            args["n_estimators"] = n
+            cls.n_estimators = n
+            print "cls.n_estimators = ", cls.n_estimators
+        cls, data = learn(Cls, args, examples, ["train"], ["devel"], False, terms, cls=cls, verbose=False)
+        performances.append({x:data["results"]["average"][x] for x in ("auc", "fscore", "precision", "recall")})
+        performances[-1]["n"] = n
+        if best == None or data["results"]["average"]["auc"] > data["results"]["average"]["auc"]:
+            best = data
+        else: # Release the not-best results
+            data = None
+    print "Warm start parameter grid search complete"
+    for performance in performances:
+        print performance["n"], "\t", metricsToString(performance)
+    return best
 
 def optimize(classifier, classifierArgs, examples, cvJobs=1, terms=None, useMultiOutputClassifier=False, outDir=None, negatives=False, useTestSet=False, useCAFASet=False):
-    #grid = ParameterGrid({"n_estimators":[10], "n_jobs":[n_jobs], "verbose":[verbose]}) #{"n_estimators":[1,2,10,50,100]}
-    #XTrainAndDevel, XTest, yTrainAndDevel, yTest = train_test_split(X, y, test_size=0.2, random_state=0)
-    #XTrain, XDevel, yTrain, yDevel = train_test_split(XTrainAndDevel, yTrainAndDevel, test_size=0.2, random_state=0)
-#     sets = examples["sets"]
-#     trainIndices = [i for i in range(len(sets)) if "train" in sets[i]]
-#     develIndices = [i for i in range(len(sets)) if "devel" in sets[i]]
-#     trainFeatures = examples["features"][trainIndices]
-#     develFeatures = examples["features"][develIndices]
-#     trainLabels = examples["labels"][trainIndices]
-#     develLabels = examples["labels"][develIndices]
-#     develIds = [examples["ids"][i] for i in range(len(sets)) if "devel" in sets[i]]
-#     print "Optimizing, train / devel = ", trainFeatures.shape[0], "/", develFeatures.shape[0]
-#     if useMultiOutputClassifier:
-#         print "Using MultiOutputClassifier"
     best = None
-    print "Parameter grid search", time.strftime('%X %x %Z')
+    print "Parameter grid search"
     Cls = importNamed(classifier)
-    #grid = parseOptions(classifierArgs)
-    for args in ParameterGrid(classifierArgs):
-        data = learn(Cls, args, examples, ["train"], ["devel"], useMultiOutputClassifier, terms)
-#         print "Learning with args", args
-#         cls = Cls(**args)
-#         if useMultiOutputClassifier:
-#             cls = MyMultiOutputClassifier(cls) # OneVsRestClassifier(cls)
-#         cls.fit(trainFeatures, trainLabels)
-#         print "Predicting the devel set"
-#         predicted = cls.predict(develFeatures)
-#         #score = roc_auc_score(develLabels, predicted, average="micro")
-#         #scores = roc_auc_score(develLabels, predicted, average=None)
-#         #print "Average =", score
-#         #results = getResults(examples, scores, terms)
-#         results = evaluate(develLabels, predicted, examples["label_names"], examples["label_size"], terms)
-#         print "Average:", metricsToString(results["average"])
-#         print getResultsString(results, 20, ["average"])
-        if best == None or data["results"]["average"]["auc"] > data["results"]["average"]["auc"]:
-            best = data #{"results":results, "args":args, "predicted":predicted, "gold":develLabels}
-            #if hasattr(cls, "feature_importances_"):
-            #    best["feature_importances"] = cls.feature_importances_
-        print time.strftime('%X %x %Z')
-#     develResultPath = os.path.join(outDir, "test-predictions.tsv")
-#     testResultPath = os.path.join(outDir, "test-predictions.tsv")
-#     testResultPath = os.path.join(outDir, "test-predictions.tsv")
+    if classifierArgs.get("warm_start") == [True]:
+        best = warmStartGrid(Cls, classifierArgs, examples, terms)
+    else:
+        for args in ParameterGrid(classifierArgs):
+            _, data = learn(Cls, args, examples, ["train"], ["devel"], useMultiOutputClassifier, terms)
+            if best == None or data["results"]["average"]["auc"] > data["results"]["average"]["auc"]:
+                best = data #{"results":results, "args":args, "predicted":predicted, "gold":develLabels}
+            else: # Release the not-best results
+                data = None
     print "Best classifier arguments:", best["args"]
     print "Best development set results:", metricsToString(best["results"]["average"])
     if outDir != None:
         saveResults(best, os.path.join(outDir, "devel"), examples["label_names"], negatives=negatives)
     if useTestSet:
         print "Classifying the test set"
-        data = learn(Cls, best["args"], examples, ["train", "devel"], ["test"], useMultiOutputClassifier, terms)
+        _, data = learn(Cls, best["args"], examples, ["train", "devel"], ["test"], useMultiOutputClassifier, terms)
         if outDir != None:
             saveResults(data, os.path.join(outDir, "test"), examples["label_names"], negatives=negatives)
     if useCAFASet:
         print "Classifying the CAFA targets"
-        data = learn(Cls, best["args"], examples, ["train", "devel", "test"], ["cafa"], useMultiOutputClassifier, terms)
+        _, data = learn(Cls, best["args"], examples, ["train", "devel", "test"], ["cafa"], useMultiOutputClassifier, terms)
         if outDir != None:
             saveResults(data, os.path.join(outDir, "cafa"), examples["label_names"], negatives=negatives)
-#     if outDir != None:
-#         saveResults(best["results"], os.path.join(outDir, "devel-results.tsv"))
-#         savePredictions(develIds, develLabels, best["predicted"], examples["label_names"], os.path.join(outDir, "devel-predictions.tsv"))
-#    return best
-    #clf = GridSearchCV(RandomForestClassifier(), args, verbose=verbose, n_jobs=cvJobs, scoring=scoring)
-    #clf.fit(X, y)
-    #print "Best params", (clf.best_params_, clf.best_score_)
 
 # def learn(train, devel, test, limit=None, limitTerms=None, featureGroups=None):
 #     print time.strftime('%X %x %Z')

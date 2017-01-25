07af5615d499311ec544d4b8a16d6313d89b1f7e
jari <jari.bjorne@utu.fi>
2017-01-25 18:33:05 +0200

    Working on the 10-fold division.

--- makeFolds.py
+++ makeFolds.py
@@ -2,23 +2,41 @@ import os
 import loading
 from collections import defaultdict
 import random
+import gzip
+from csv import DictWriter
+
+def saveFolds(proteins, outPath):
+    rows = []
+    for key in sorted(proteins.keys()):
+        protein = proteins[key]
+        rows.append({"id":key, "fold":protein.get("fold"), "split":protein.get("split")})
+    with gzip.open(outPath, "wt") as f:
+        dw = DictWriter(f, delimiter="\t", columns=["id", "fold", "split"])
+        dw.writeheader()
+        dw.writerows(rows)
 
 def makeFolds(proteins):
     rand = random.Random(1)
+    folds = range(10)
     for key in sorted(proteins.keys()):
-        rand.
+        protein = proteins[key]
+        if protein.get("split") == None:
+            continue
+        protein["fold"] = rand.choice(folds)
 
-def run(dataPath, outDir=None):
+def run(dataPath, outPath=None):
     print "==========", "Generating Folds", "=========="
     proteins = defaultdict(lambda: dict())
     print "Loading Swissprot proteins"
     loading.loadFASTA(os.path.join(options.dataPath, "Swiss_Prot", "Swissprot_sequence.tsv.gz"), proteins)
-    print "Loading CAFA3 targets"
-    loading.loadFASTA(os.path.join(options.dataPath, "CAFA3_targets", "Target_files", "target.all.fasta"), proteins, True)
-    print "Proteins:", len(proteins)
-    termCounts = loading.loadAnnotations(os.path.join(options.dataPath, "data", "Swissprot_propagated.tsv.gz"), proteins)
-    print "Unique terms:", len(termCounts)
+    #print "Loading CAFA3 targets"
+    #loading.loadFASTA(os.path.join(options.dataPath, "CAFA3_targets", "Target_files", "target.all.fasta"), proteins, True)
+    #print "Proteins:", len(proteins)
+    #termCounts = loading.loadAnnotations(os.path.join(options.dataPath, "data", "Swissprot_propagated.tsv.gz"), proteins)
+    #print "Unique terms:", len(termCounts)
     loading.loadSplit(os.path.join(options.dataPath, "data"), proteins)
+    makeFolds(proteins)
+    saveFolds(proteins, outPath)
 
 if __name__=="__main__":       
     from optparse import OptionParser

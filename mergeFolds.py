11c84444ba5110b515cb4527fc99f3821697083d
jari <jari.bjorne@utu.fi>
2017-01-29 21:37:31 +0200

    More work on merging the cross-validation results.

--- mergeFolds.py
+++ mergeFolds.py
@@ -1,5 +1,8 @@
 import os
 import gzip
+import operator
+import filecmp
+import shutil
 
 def onError(message, errors):
     if errors == "strict":
@@ -10,14 +13,11 @@ def onError(message, errors):
 def getFoldDirs(inPath, foldPattern, numFolds):
     return [(i, os.path.join(inPath, foldPattern.replace("{NUMBER}", str(i)))) for i in range(numFolds)]        
 
-def collect(inPath, numFolds, foldPattern, errors):
-    #predictions = {"devel":[], "test":[]}
-    #seenIds = {"devel":set(), "test":set()}
-    outFiles = {}
-    for setName in ("devel", "test"):
-        outFiles[setName] = gzip.open(os.path.join(inPath, setName + "allfolds-predicted.tsv.gz"))
-    logLines = {i:{"cv":None, "command":None, "best_args":None, "avg_devel":None, "avg_test":None} for i in range(numFolds)}
-    patterns = {"cv":"Cross-validation sets", "command":"Command line:", "best_args":""}
+def readLogs(inPath, foldPattern, numFolds, errors):
+    print "Reading log files"
+    patternMatch = {"Cross-validation sets":"cv", "Command line:":"command", "best_args":"DFG", "avg_devel":"SDF", "avg_test":"DF"}
+    patterns = sorted(patternMatch.keys())
+    logLines = {i:{key:None for key in patterns} for i in range(numFolds)} 
     for i, foldDir in getFoldDirs(inPath, foldPattern, numFolds):
         logPath = os.path.join(foldDir, "log.txt")
         if not os.path.exists(logPath):
@@ -25,7 +25,38 @@ def collect(inPath, numFolds, foldPattern, errors):
             continue
         with open(logPath, "rt") as f:
             for line in f:
-                if line.
+                for pattern in patterns:
+                    if pattern in line:
+                        assert logLines[i][patternMatch[pattern]] == None
+                        logLines[i][patternMatch[pattern]] = line.strip()
+    s = ""
+    argFrequency = {}
+    for i in range(numFolds):
+        for pattern in patterns:
+            s += "***", pattern, "***"
+            s += str(i) + ":\t" + str(logLines[i]) + "\n"
+        argLine = logLines[i]["best_args"]
+        if argLine != None:
+            argString = argLine.split("\t")[-1].strip()
+            if argString not in argFrequency:
+                argFrequency[argString] = 0
+            argFrequency[argString] += 1
+    freqs = sorted([(argFrequency[key], key) for key in argFrequency], reverse=True)
+    maxHits = freqs[0]
+    mostCommonArgs = max(argFrequency.iteritems(), key=operator.itemgetter(1))[0]
+    s += "Best arguments frequency"
+    for argFrequency in 
+    return s, argFrequency[0][0]
+
+def collect(inPath, numFolds, foldPattern, errors):
+    #predictions = {"devel":[], "test":[]}
+    #seenIds = {"devel":set(), "test":set()}
+    outFiles = {}
+    for setName in ("devel", "test"):
+        outFiles[setName] = gzip.open(os.path.join(inPath, setName + "allfolds-predicted.tsv.gz"))
+    logText, mostCommonArgsFolds = readLogs(inPath, foldPattern, numFolds, errors)
+    with open(os.path.join(inPath, "logs.txt"), "wt") as f:
+        f.write(logText)
         
     for i, foldDir in getFoldDirs(inPath, foldPattern, numFolds):
         foldDir = os.path.join(inPath, foldPattern.replace("{NUMBER}", str(i)))
@@ -42,6 +73,13 @@ def collect(inPath, numFolds, foldPattern, errors):
                         outFiles[setName].write(line)
             else:
                 onError("Result file '" + predPath + "' not found")
+        if i in mostCommonArgsFolds:
+            foldCAFAPath = os.path.join(foldDir, "cafa-predictions.tsv.gz")
+            finalCAFAPath = os.path.join(inPath, "cafa-allfolds-predicted.tsv.gz")
+            if os.path.exists(finalCAFAPath):
+                assert filecmp(foldCAFAPath, finalCAFAPath)
+            else:
+                shutil.copy2(foldCAFAPath, finalCAFAPath)
         
 
 def run(inPath, numFolds):

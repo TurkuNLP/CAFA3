cafe101e5b5ba94b42d213cb96697ed687a8fe06
jari <jari.bjorne@utu.fi>
2017-02-03 03:38:47 +0200

    Allow validation between any pair of TSV files and CAFA target directories. Fixed a bug that caused non-cafa target rows to not be skipped.

--- validate.py
+++ validate.py
@@ -3,14 +3,14 @@ from collections import defaultdict
 import gzip
 import csv
 
-def loadCAFA(cafaDir, modelNumber, baselineConf, termTag, counts):
+def loadCAFA(cafaDir, modelNumber, baselineConf, termTag, counts, tag):
     modelTag = "_" + str(modelNumber) + "_"
     proteins = {}
     for filename in os.listdir(cafaDir):
         filePath = os.path.join(cafaDir, filename)
         if modelTag in filePath:
             print "Reading", filePath
-            counts["model-files-cafa"] += 1
+            counts["model-files-" + tag] += 1
             with open(os.path.join(filePath), "rt") as f:
                 line = f.readline()
                 assert line.startswith("AUTHOR")
@@ -24,21 +24,21 @@ def loadCAFA(cafaDir, modelNumber, baselineConf, termTag, counts):
                     cafaId, termId, confidence = line.strip().split()
                     rowTermTag = termId.split(":")[0]
                     if rowTermTag != termTag:
-                        counts["skipped-term-tag-cafa-" + rowTermTag] += 1
+                        counts["skipped-term-tag-" + tag + rowTermTag] += 1
                         continue
                     if cafaId not in proteins:
                         proteins[cafaId] = {}
                     if termId in proteins[cafaId]:
-                        counts["duplicate-terms-cafa"] += 1
+                        counts["duplicate-terms-" + tag] += 1
                     proteins[cafaId][termId] = confidence
-                    counts["terms-total-cafa"] += 1
+                    counts["terms-total-" + tag] += 1
                     if confidence == baselineConf:
-                        counts["terms-baseline-conf-cafa"] += 1
+                        counts["terms-baseline-conf-" + tag] += 1
                     else:
-                        counts["terms-non-baseline-conf-cafa"] += 1
+                        counts["terms-non-baseline-conf-" + tag] += 1
     return proteins
 
-def loadTSV(tsvPath, baselineConf, counts):
+def loadTSV(tsvPath, baselineConf, counts, tag):
     proteins = {}
     rowCount = 0
     with gzip.open(tsvPath, "rt") as f:
@@ -48,11 +48,12 @@ def loadTSV(tsvPath, baselineConf, counts):
             if rowCount % 1000000 == 0:
                 print "Processing row", rowCount
             if row["cafa_ids"] in ("", None):
-                counts["proteins-non-cafa-target-tsv"] += 1
+                counts["skipped-terms-non-cafa-target-" + tag] += 1
+                continue
             cafaIds = row["cafa_ids"].split(",")
             cafaIds = [x for x in cafaIds if not x.startswith("M")]
             if len(cafaIds) == 0:
-                counts["moonlight-only-predictions-tsv"] += 1
+                counts["moonlight-only-predictions-" + tag] += 1
                 continue
             assert len(cafaIds) == 1, cafaIds
             cafaId = cafaIds[0]
@@ -62,63 +63,71 @@ def loadTSV(tsvPath, baselineConf, counts):
                 termId = row["label"]
                 confidence = '%.2f' % float(row["confidence"])
                 if termId in proteins[cafaId]:
-                    counts["duplicate-terms-tsv"] += 1
+                    counts["duplicate-terms-" + tag] += 1
                 proteins[cafaId][termId] = confidence
-                counts["terms-total-tsv"] += 1
+                counts["terms-total-" + tag] += 1
                 if confidence == baselineConf:
-                    counts["terms-baseline-conf-tsv"] += 1
+                    counts["terms-baseline-conf-" + tag] += 1
                 else:
-                    counts["terms-non-baseline-conf-tsv"] += 1
+                    counts["terms-non-baseline-conf-" + tag] += 1
     return proteins
 
-def validate(cafaDir, tsvPath, modelNumber, baselineConf, termTag):
+def loadSource(source, modelNumber, baselineConf, termTag, counts, tag):
+    if os.path.isdir(source):
+        return loadCAFA(source, modelNumber, baselineConf, termTag, counts, tag)
+    else:
+        return loadTSV(source, baselineConf, counts, tag)
+
+def validate(sourceA, sourceB, modelNumber, baselineConf, termTag):
     counts = defaultdict(int)
-    print "Reading CAFA proteins"
-    proteins1 = loadCAFA(cafaDir, modelNumber, baselineConf, termTag, counts)
-    print "Reading TSV proteins"
-    proteins2 = loadTSV(tsvPath, baselineConf, counts)
-    counts["proteins-cafa"] = len(proteins1)
-    counts["proteins-tsv"] = len(proteins2)
-    allProteinKeys = sorted(set(proteins1.keys() + proteins2.keys()))
+    print "Reading A proteins from", sourceA
+    proteinsA = loadSource(sourceA, modelNumber, baselineConf, termTag, counts, "A")
+    print "Reading B proteins from", sourceB
+    proteinsB = loadSource(sourceB, modelNumber, baselineConf, termTag, counts, "B")
+    counts["proteins-A"] = len(proteinsA)
+    counts["proteins-B"] = len(proteinsB)
+    allProteinKeys = sorted(set(proteinsA.keys() + proteinsB.keys()))
     numProteinKeys = len(allProteinKeys)
     protCount = 0
-    for key in allProteinKeys:
+    for protId in allProteinKeys:
         protCount += 1
         if protCount % 10000 == 0:
-            print "Processing protein", key, "(" + str(protCount) + "/" + str(numProteinKeys) + ")"
-        if key not in proteins1:
-            counts["protein-missing-cafa"] += 1
-        if key not in proteins2:
-            counts["protein-missing-tsv"] += 1
-        prot1 = proteins1.get(key, {})
-        prot2 = proteins2.get(key, {})
-        for label in sorted(set(prot1.keys() + prot2.keys())):
-            conf1 = prot1.get(label)
-            conf2 = prot2.get(label)
-            if conf1 != None and conf1 != None:
+            print "Processing protein", protId, "(" + str(protCount) + "/" + str(numProteinKeys) + ")"
+        if protId not in proteinsA:
+            counts["protein-missing-A"] += 1
+        if protId not in proteinsB:
+            counts["protein-missing-B"] += 1
+        protA = proteinsA.get(protId, {})
+        protB = proteinsB.get(protId, {})
+        for label in sorted(set(protA.keys() + protB.keys())):
+            confA = protA.get(label)
+            confB = protB.get(label)
+            if confA != None and confB != None:
                 counts["matching-terms-total"] += 1
-                if conf1 != conf2:
+                if confA != confB:
                     counts["matching-terms-different-conf"] += 1
                     #print key, label, (conf1, conf2)
-                elif conf1 == baselineConf:
+                elif confA == baselineConf:
+                    assert confB == baselineConf
                     counts["matching-terms-baseline-conf"] += 1
                 else:
+                    assert confA == confB
                     counts["matching-terms-non-baseline-conf"] += 1
-            elif conf1 == None:
-                counts["missing-terms-total-cafa"] += 1
-                if conf2 == baselineConf:
-                    counts["missing-baseline-conf-terms-cafa"] += 1
+            elif confA == None:
+                counts["missing-terms-total-A"] += 1
+                if confB == baselineConf:
+                    counts["missing-baseline-conf-terms-A"] += 1
                 else:
-                    counts["missing-non-baseline-conf-terms-cafa"] += 1
-            elif conf2 == None:
-                counts["missing-terms-total-tsv"] += 1
-                if conf1 == baselineConf:
-                    counts["missing-baseline-conf-terms-tsv"] += 1
+                    counts["missing-non-baseline-conf-terms-A"] += 1
+            elif confB == None:
+                counts["missing-terms-total-B"] += 1
+                if confA == baselineConf:
+                    counts["missing-baseline-conf-terms-B"] += 1
                 else:
-                    counts["missing-non-baseline-conf-terms-tsv"] += 1
+                    counts["missing-non-baseline-conf-terms-B"] += 1
     print "Statistics for model number", modelNumber
-    print "CAFA model directory:", cafaDir
-    print "TSV file:", tsvPath
+    print "Source A:", sourceA
+    print "Source B:", sourceB
     for key in sorted(counts.keys()):
         print key + ": " + str(counts[key])
 

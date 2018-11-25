048ccf0575760d7ff4aa406a10063a77d5283c8a
jari <jari.bjorne@utu.fi>
2018-11-25 22:40:08 +0200

    The input files can be sorted.

--- utils/mergeCSV.py
+++ utils/mergeCSV.py
@@ -4,26 +4,39 @@ import gzip
 import csv
 import Stream
 
-def readCSVs(pathPattern, idName, delimiter="\t"):
+def readCSVs(pathPattern, columns, sortBy=None, delimiter="\t"):
     rows = []
     rowById = {}
+    idName = getIdName(columns)
     for pathName in glob.glob(pathPattern):
         print "Loading input file", pathName
         #fileName = os.path.basename(pathName)
         with (gzip.open(pathName, "rt") if pathName.endswith(".gz") else open(pathName, "rt")) as f:
             reader = csv.DictReader(f, delimiter=delimiter)
+            fileRows = []
             for line in reader:
                 rowId = line[idName]
                 if rowId not in rowById:
                     rowById[rowId] = {key:[] for key in line.keys()}
+                    if sortBy != None:
+                        rowById[rowId]["rank"] = []
                     rows.append(rowById[rowId])
                 row = rowById[rowId]
                 for key in line:
                     if key != idName:
-                        row[key].append(line[key])
+                        row[key].append(columns[key]["type"](line[key]))
                     else:
                         row[key] = idName
                 rowById[rowId] = row
+                fileRows.append(line)
+            if sortBy != None:
+                print "Sorting by column", sortBy
+                fileRows.sort(key=lambda x: x[sortBy], reverse=True)
+                rank = 0
+                for fileRow in fileRows:
+                    fileRow["rank"] = rank
+                    rowById[fileRow[idName]]["rank"].append(rank)
+                    rank += 1
     return rows
 
 def mergeRows(rows, columns):
@@ -32,13 +45,12 @@ def mergeRows(rows, columns):
     for row in rows:
         for key in keys:
             if key not in columns or columns[key]["action"] == "mean":
-                row[key] = sum([columns[key]["type"](x) for x in row[key]]) / len(row[key])
+                row[key] = sum(x for x in row[key]) / len(row[key])
             elif columns[key]["action"] == "id":
                 pass
             elif columns[key]["action"] == "range":
-                values = [columns[key]["type"](x) for x in row[key]]
-                minVal = min(values)
-                maxVal = max(values)
+                minVal = min(row[key])
+                maxVal = max(row[key])
                 if (minVal != maxVal):
                     row[key] = str(minVal) + "-" + str(maxVal)
                 else:
@@ -60,7 +72,7 @@ def getColumns(columnPattern):
             columns[split[0]] = {"action":split[1], "type":eval(split[2])}
     return columns
 
-def validateColumns(columns):
+def getIdName(columns):
     print "Validating the column rules:", columns
     idName = None
     for key in columns:
@@ -70,11 +82,10 @@ def validateColumns(columns):
     assert idName != None
     return idName
 
-def mergeCSVs(pathPattern, outPath, columns, delimiter="\t"):
+def mergeCSVs(pathPattern, outPath, columns, sortBy=None, delimiter="\t"):
     Stream.openLog(outPath + "-log.txt")
     columns = getColumns(columns)
-    idName = validateColumns(columns)
-    rows = readCSVs(pathPattern, idName, delimiter=delimiter)
+    rows = readCSVs(pathPattern, columns, sortBy=sortBy, delimiter=delimiter)
     if len(rows) == 0:
         print "No input rows"
         return
@@ -92,7 +103,8 @@ if __name__=="__main__":
     optparser.add_option("-i", "--input", help="Glob pattern. Remember to quote so the shell doesn't expand it!")
     optparser.add_option("-o", "--output", help="Output file path")
     optparser.add_option("-c", "--columns", help="Comma-separated list of column:action[:type] duplets/triplets, e.g. 'mycolumn:mean:float'.")
+    optparser.add_option("-s", "--sortBy", default=None, help="Column to sort by (generates the 'rank' column)")
     optparser.add_option("-d", "--delimiter", default="\t", help="CSV file delimiter")
     (options, args) = optparser.parse_args()
     
-    mergeCSVs(options.input, options.output, options.columns, options.delimiter)
+    mergeCSVs(options.input, options.output, options.columns, options.sortBy, options.delimiter)
